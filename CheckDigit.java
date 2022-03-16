a72a7cf943ff323c8d6559f6bb342eb37c3bfc0b
ojas604 <98620101+ojas604@users.noreply.github.com>
2022-03-16 14:34:24 -0400

    Add files via upload

---
+++ CheckDigit.java
@@ -0,0 +1,57 @@
+/*************************************************************************
+ *  Compilation:  javac CheckDigit.java
+ *  Execution:    java CheckDigit 020131452
+ *
+ *  @author: Ojas Jain, oj34, oj34@scarletmail.rutgers.edu
+ *
+ *  Takes a 12 or 13 digit integer as a command line argument, then computes
+ *  and displays the check digit
+ *
+ *  java CheckDigit 048231312622
+ *  0
+ *
+ *  java CheckDigit 9780470458310
+ *  0
+ * 
+ *  java CheckDigit 9780470454310
+ *  8
+ * 
+ *  Print only the check digit character, nothing else.
+ *
+ *************************************************************************/
+
+public class CheckDigit {
+
+     public static void main(String[] args)     {
+        
+                int sum1 = 0;
+                int sum2 = 0;      
+                long num = Long.parseLong(args[0]);
+                int count = 0;
+                
+                while(num>0) {
+        
+                    if (count % 2 == 0)
+        
+                        sum1 += num % 10;
+        
+                    else {
+                        sum2 += num % 10;
+                    }
+        
+                    num /= 10;
+                    count++;
+        
+                }
+
+                sum1 %= 10;
+                sum2 %= 10;
+                sum2 *= 3;
+                sum2 %= 10;
+                int digit = sum1+sum2;
+                digit %= 10;        
+                System.out.println(digit);
+        
+            }
+        
+}    
